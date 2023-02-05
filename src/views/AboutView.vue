e8485143247296fde3006ac293d3dcd38f56fd1f
alexandru_marincas <alexandru.marincas@yahoo.ro>
2023-02-05 18:51:06 +0200

    removed gh-pages, added details about site

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>IRON WELDING</h1>
+    <h3>pentru noi metalul este doar o plastilina, il modelam cum vrem</h3>
   </div>
 </template>
+
+<style lang="scss" scoped>
+
+.about{
+  h1{
+    color: #464646;
+  }
+  h3{
+    color: #42b983;
+    font-family: 'Dancing Script', cursive;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Site <span>under</span> construction</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.home{
+  h1{
+    span{
+      color: #fac8cf;
+      text-transform: uppercase;
+    }
+  }
+}
+</style>
